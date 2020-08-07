7015818220d1d5dc53e4d3b174c049a6691869dd
mhlee <mhlee@ezfarm.co.kr>
2020-08-07 17:39:11 +0900

    Egg ETL 로직 최초 등록 #1

---
+++ src/main/java/newegg/EggEtlApplication.java
@@ -0,0 +1,13 @@
+package newegg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggEtlApplication.class, args);
+	}
+
+}

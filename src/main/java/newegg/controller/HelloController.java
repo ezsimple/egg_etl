ee188be2f2b5e2f9b25c0b4e0a13de23be846f1d
mhlee <mhlee@ezfarm.co.kr>
2020-08-10 16:25:30 +0900

    1. profile - default 추가 2. springboot - mybatis 연동 작업중 3. project 를 war 로 변경    - Servelet Initialize 필요

--- src/main/java/newegg/controller/HelloController.java
+++ src/main/java/newegg/controller/HelloController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import io.mkeasy.resolver.CommandMap;
+import io.mkeasy.webapp.processor.QueryFactory;
 import lombok.extern.slf4j.Slf4j;
+import newegg.utils.QueryMap;
 
 @Slf4j
 @Controller
@@ -21,8 +23,18 @@ class HelloController {
 	@ResponseBody
 	@RequestMapping(value= {"/", "/hello.do"})
 	public String hello(ModelMap model, CommandMap commandMap) throws Exception {
+		
+		commandMap.debugParams();
+
 		String message = "{'message' : '안녕! 꼬꼬 ETL'}";
 		log.debug("{}", message);
+		
+		String ns = "newegg.hello.test";
+		String nsId = "selectFromDual";
+		QueryMap queryMap = new QueryMap();
+
+		QueryFactory.execute(ns, nsId, queryMap);
+		
 		return message;
 	}
 	
